19a2718733f73d5c9cbe317c546a8be7d455c16e
pavankumar kalluri <jyotshna12121999@gmail.com>
2020-07-13 16:21:10 +0530

    updated

--- precache-manifest.fff81e45e1a103afeb2395fab47f04b4.js
+++ precache-manifest.fff81e45e1a103afeb2395fab47f04b4.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "94e0033b01d52a54d60bd5851817b2cb",
-    "url": "/react-app/index.html"
+    "url": "/react/resume.html"
   },
   {
     "revision": "3bdd201f9561e0f24c65",
-    "url": "/react-app/static/css/main.ab003eb6.chunk.css"
+    "url": "/react/static/css/main.ab003eb6.chunk.css"
   },
   {
     "revision": "7ea7fa3654f44c214f5f",
-    "url": "/react-app/static/js/2.80fda868.chunk.js"
+    "url": "/react/static/js/2.80fda868.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react-app/static/js/2.80fda868.chunk.js.LICENSE.txt"
+    "url": "/react/static/js/2.80fda868.chunk.js.LICENSE.txt"
   },
   {
     "revision": "3bdd201f9561e0f24c65",
-    "url": "/react-app/static/js/main.a5e63f2e.chunk.js"
+    "url": "/react/static/js/main.a5e63f2e.chunk.js"
   },
   {
     "revision": "e2c6b0a16d423a796dce",
-    "url": "/react-app/static/js/runtime-main.9a102076.js"
+    "url": "/react/static/js/runtime-main.9a102076.js"
   },
   {
     "revision": "4948508ccfd3552d72bc0783dfe5502f",
-    "url": "/react-app/static/media/delivery-man.4948508c.svg"
+    "url": "/react/static/media/delivery-man.4948508c.svg"
   }
 ]);
